c64fc4b4b991a4e3fd6b2203312501791f370c42
Luke Imhoff <luke_imhoff@rapid7.com>
2014-05-01 09:36:37 -0500

    Simplecov + Coveralls for cucumber
    MSP-9895

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,15 @@
+# simplecov must be loaded first as only files required after simplecov will show coverage data
+require 'simplecov'
+require 'coveralls'
+
+if ENV['TRAVIS'] == 'true'
+  # don't generate local report as it is inaccessible on travis-ci, which is why coveralls is being used.
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+else
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      # either generate the local report
+      SimpleCov::Formatter::HTMLFormatter
+  ]
+end
+
 require 'aruba/cucumber'
