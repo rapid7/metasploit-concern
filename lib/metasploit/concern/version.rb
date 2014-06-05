8bab1350cf9b9172254ed655fdc6409997a81c02
Lance Sanchez <lance_sanchez@rapid7.com>
2014-06-05 11:54:10 -0500

    removing the prerelease tag from version.rb

--- lib/metasploit/concern/version.rb
+++ lib/metasploit/concern/version.rb
@@ -8,8 +8,6 @@ module Metasploit
       MINOR = 1
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 0
-      # The prelease name, scoped to the {PATCH} version number.
-      PRERELEASE = 'use-metasploit-concern-in-pro'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
