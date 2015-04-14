7ccd60e36ad5c39e8b095bc1f9d87dba69cbe2ff
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-14 12:25:54 -0500

    Update version for branch
    MSP-12550

--- lib/metasploit/concern/version.rb
+++ lib/metasploit/concern/version.rb
@@ -5,9 +5,11 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 3
+      MINOR = 4
       # The patch number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 1
+      PATCH = 0
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+      PRERELEASE = 'app-concerns-eager-load'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the `PRERELEASE` in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
